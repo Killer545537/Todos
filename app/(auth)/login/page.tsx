40df9f6af35640e0d50d90eebe40cc32c4c299b7
Killer545537 <srajanmahajan1@gmail.com>
2025-10-27 16:38:15 +0530

    feat(auth): added automatic redirects if logged in

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,27 @@
 import type { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 import LoginForm from '@/components/auth/login-form';
+import { getId } from '@/helpers/auth';
 
 export const metadata: Metadata = {
     title: 'Login Page',
     description: 'Log in to your account to get started.',
 };
 
-const LoginPage = () => (
-    <div className='bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
-        <div className='w-full max-w-sm'>
-            <LoginForm />
+const LoginPage = async () => {
+    const id = await getId();
+
+    if (id) {
+        redirect('/todos');
+    }
+
+    return (
+        <div className='bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
+            <div className='w-full max-w-sm'>
+                <LoginForm />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default LoginPage;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -5,14 +5,24 @@ export const metadata: Metadata = {
     description: 'Create a new account to get started.',
 };
 
+import { redirect } from 'next/navigation';
 import SignUpForm from '@/components/auth/sign-up-form';
+import { getId } from '@/helpers/auth';
 
-const SignUpPage = () => (
-    <div className='bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
-        <div className='w-full max-w-sm'>
-            <SignUpForm />
+const SignUpPage = async () => {
+    const id = await getId();
+
+    if (id) {
+        redirect('/todos');
+    }
+
+    return (
+        <div className='bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
+            <div className='w-full max-w-sm'>
+                <SignUpForm />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default SignUpPage;
